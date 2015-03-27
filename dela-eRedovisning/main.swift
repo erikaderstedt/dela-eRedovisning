6bca8bb3dbecb7d15479c94d7740018a7e5de3f4
erikaderstedt <erik@aderstedtsoftware.com>
2015-03-27 10:08:14 +0100

    Initial Commit

---
+++ dela-eRedovisning/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  dela-eRedovisning
+//
+//  Created by Erik Aderstedt on 2015-03-27.
+//  Copyright (c) 2015 Aderstedt Software AB. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
