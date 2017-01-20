bce1ca810e8793de2c431beae1ce3dbadf01072d
Erik Aderstedt <erik@aderstedtsoftware.com>
2017-01-20 10:40:21 +0100

    Uppdateringar för Swift 3.

--- dela-eRedovisning/main.swift
+++ dela-eRedovisning/main.swift
@@ -9,17 +9,19 @@
 import Foundation
 import Quartz
 
-func accountOnPage(page: PDFPage) -> String? {
+// Get Safari to download PDFs:
+// defaults write com.apple.Safari WebKitOmitPDFSupport -bool YES
+
+func accountOnPage(_ page: PDFPage) -> String? {
     
     // Look for line with "Kontonr", and then get the next line that doesn't match "Utg".
 
-    let s = page.string()
+    let s = page.string
     let accountnumber : NSRegularExpression? = try? NSRegularExpression(pattern: "^[0-9 ]+-[0-9]+ (SEK|EUR)$", options: [])
-    let lines = s.componentsSeparatedByCharactersInSet(NSCharacterSet.newlineCharacterSet())
+    let lines = s?.components(separatedBy: CharacterSet.newlines)
     
-    for line in lines {
-        let m = accountnumber?.matchesInString(line, options: [], range: NSMakeRange(0, line.characters.count))
-        if m?.count > 0 {
+    for line in lines! {
+        if let m = accountnumber?.matches(in: line, options: [], range: NSMakeRange(0, line.characters.count)), m.count > 0 {
             return line
         }
     }
@@ -27,21 +29,21 @@ func accountOnPage(page: PDFPage) -> String? {
     return nil
 }
 
-func numberOnPage(page: PDFPage) -> String? {
+func numberOnPage(_ page: PDFPage) -> String? {
     
-    let s = page.string()
-    let lines = s.componentsSeparatedByCharactersInSet(NSCharacterSet.newlineCharacterSet())
+    let s = page.string
+    let lines = s?.components(separatedBy: CharacterSet.newlines)
     let snumber : NSRegularExpression? = try? NSRegularExpression(pattern: "^Nummer ?([0-9]+)?", options: [])
     var getTheNextOne = false
-    for line in lines {
+    for line in lines! {
         if getTheNextOne {
             return line
         }
-        let m = snumber?.matchesInString(line, options: [], range: NSMakeRange(0, line.characters.count))
+        let m = snumber?.matches(in: line, options: [], range: NSMakeRange(0, line.characters.count))
 		if let r = m?.first {
-            if r.rangeAtIndex(1).length > 0 {
+            if r.rangeAt(1).length > 0 {
                 let q = line as NSString
-                return (q.substringWithRange(r.rangeAtIndex(1)) as String)
+                return (q.substring(with: r.rangeAt(1)) as String)
             }
             getTheNextOne = true
         }
@@ -49,27 +51,27 @@ func numberOnPage(page: PDFPage) -> String? {
     return nil
 }
 
-if Process.arguments.count < 2 {
+if CommandLine.arguments.count < 2 {
     print("dela-eRedovisning <filnamn> ...")
     exit(1)
 }
 
-let c = Process.arguments.count
-for argument in Process.arguments[1..<c] {
-    if let inputDoc = PDFDocument(URL: NSURL(fileURLWithPath: argument)) {
-        let pageCount = inputDoc.pageCount()
+let c = CommandLine.arguments.count
+for argument in CommandLine.arguments[1..<c] {
+    if let inputDoc = PDFDocument(url: URL(fileURLWithPath: argument)) {
+        let pageCount = inputDoc.pageCount
         var outputDoc : PDFDocument? = nil
         var currentAccount : String = ""
         var currentNumber : String = ""
         
         for i in 0..<pageCount {
-            let page = inputDoc.pageAtIndex(i)
+            let page = inputDoc.page(at: i)
             // We need both account and number to change document.
-            if let anum = accountOnPage(page) {
-                if let dnum = numberOnPage(page) {
+            if let anum = accountOnPage(page!) {
+                if let dnum = numberOnPage(page!) {
                     if anum != currentAccount || dnum != currentNumber {
                         if let odoc = outputDoc {
-                            odoc.writeToFile("\(currentAccount) - \(currentNumber).pdf")
+                            odoc.write(toFile: "\(currentAccount) - \(currentNumber).pdf")
                             print("Writing \(currentAccount) - \(currentNumber).pdf")
                         }
                         outputDoc = PDFDocument()
@@ -79,11 +81,11 @@ for argument in Process.arguments[1..<c] {
                 }
             }
             if let odoc = outputDoc {
-                odoc.insertPage(page, atIndex: odoc.pageCount())
+                odoc.insert(page!, at: odoc.pageCount)
             }
         }
         if let odoc = outputDoc {
-            odoc.writeToFile("\(currentAccount) - \(currentNumber).pdf")
+            odoc.write(toFile: "\(currentAccount) - \(currentNumber).pdf")
             print("Writing \(currentAccount) - \(currentNumber).pdf")
         }
     }
